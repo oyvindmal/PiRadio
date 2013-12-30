f4b65f9730de2b0e3f24895707ebfa48150f8d6f
Oyvind Malin <oyvindmal@outlook.com>
2013-12-30 07:47:23 +0100

    Added some basic styling to the ui

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php header('Content-Type: text/html; charset=UTF-8'); ?>
 <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 <script language="javascript">
 $(document).ready(
@@ -20,25 +21,30 @@ $(document).ready(
 		});
 	}
 </script>
-<h1>RadioControls</h1>
-<h2>Currently Playing</h2>
-<p id="stationName"></p>
-<p id="nowPlaying"></p>
-<h2>Controls</h2>
-<ul>
+<link rel="stylesheet" type="text/css" href="styles/style.css" />
+<h1>PiRadio</h1>
+<div id="metaData">
+	<div id="stationImage">
+		<img src="img/nopic.png">
+	</div>
+	<div id="stationInfo">
+		<p id="stationName"></p>
+		<p id="nowPlaying"></p>
+	</div>
+	<div style="clear:both;"></div>
+</div>
+<ul id="playerControls">
 	<li><a href="Play.php">Play</a></li>
 	<li><a href="Stop.php">Stop</a></li>
 </ul>
 
-
-<h2>New StationsList</h2>
-<ul>
+<ul id="stationList">
 <?php
 	include("RadioStations.php");
 	foreach($decodedStations as $station) 
 	{
 		echo "<li>";
-		echo "<a href=\"ChangeStation.php?id=" . $station->ID  . "\">" . $station->Title . "</a>";
+		echo "<p><a href=\"ChangeStation.php?id=" . $station->ID  . "\">" . $station->Title . "</a></p>";
 		echo "</li>";
 	}		
 ?>
