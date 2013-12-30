fbf32d1db902c1737ef8ae677b9c37c99617b963
Oyvind Malin <oyvindmal@outlook.com>
2013-12-30 09:05:53 +0100

    Redesigned the UI as it looked horrible

--- index.php
+++ index.php
@@ -22,35 +22,44 @@ $(document).ready(
 	}
 </script>
 <link rel="stylesheet" type="text/css" href="styles/style.css" />
-<h1>PiRadio</h1>
-<div id="metaData">
-	<div id="stationImage">
-		<img src="img/nopic.png">
-	</div>
-	<div id="stationInfo">
-		<p id="stationName"></p>
-		<p id="nowPlaying"></p>
-	</div>
-	<div style="clear:both;"></div>
+<div id="meta">
+<a href="Admin/index.php">Admin</a>
 </div>
-<ul id="playerControls">
-	<li><a href="Play.php">Play</a></li>
-	<li><a href="Stop.php">Stop</a></li>
-</ul>
+<div id="box">
+	<div id="heading">
+		<h1>PiRadio</h1>
+	</div>
+	<div id="metaData">
+		<div id="stationImage">
+			<img src="img/nopic.png">
+		</div>
+		<div id="stationInfo">
+			<p id="stationName"></p>
+			<p id="nowPlaying"></p>
+		</div>
+		
+		<div style="clear:both;"></div>
+	</div>
+	<div id="controlsBox">
+		<ul id="playerControls">
+                	<li><a href="Play.php">Play</a></li>
+	                <li><a href="Stop.php">Stop</a></li>
+                 </ul>
+	</div>
+	<div id="stationsBox">
+		<ul id="stationList">
+			<?php
+		         include("RadioStations.php");
+		         foreach($decodedStations as $station)
+       			 {
+                		echo "<li>";
+		                echo "<p><a href=\"ChangeStation.php?id=" . $station->ID  . "\">" . $station->Title . "</a></p>";
+                		echo "</li>";
+ 			 }
+			?>
+		</ul>
 
-<ul id="stationList">
-<?php
-	include("RadioStations.php");
-	foreach($decodedStations as $station) 
-	{
-		echo "<li>";
-		echo "<p><a href=\"ChangeStation.php?id=" . $station->ID  . "\">" . $station->Title . "</a></p>";
-		echo "</li>";
-	}		
-?>
-</ul>
-<h2>Meta</h2>
-<ul>
-	<li><a href="admin/index.php">Admin</a></li>
-</ul>
+	</div>
+
+</div>
 
